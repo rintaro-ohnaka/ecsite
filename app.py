eb2d42dfb999089da2e3e51b2d640deab8a3be7d
rintaro <rintaro.onaka@d2c.co.jp>
2020-07-20 18:03:33 +0900

    Fix: 関数名の説明を記述

--- app.py
+++ app.py
@@ -8,22 +8,25 @@ import mysql.connector
 from model.const import DB
 from model.user import User
 
+# DBに接続
 def get_connection():
     cnx = mysql.connector.connect(host=DB["DB_HOST"], user=DB["DB_USER_NAME"], password=DB["DB_PASSWORD"], database=DB["DB_NAME"])
     cursor = cnx.cursor()
     return cursor, cnx
 
+# アカウント新規作成ページを表示
 @app.route("/", methods=["GET", "POST"])
 def show_user_registration_page():
     flash("ようこそ、新規登録ページへ")
     return render_template("user_registration.html")
 
-
+# ユーザー登録リクエストを受ける
 def user_registration_request():
     user_name = request.form.get("user_name", "")
     user_pass = request.form.get("user_pass", "")
     return user_name, user_pass
 
+# アカウント名とパスワードが正しく入力されているかチェック
 def is_valid_registration_request(user_name, user_pass):
     cursor, cnx = get_connection()
     if user_name == "" or user_pass == "":
@@ -34,6 +37,7 @@ def is_valid_registration_request(user_name, user_pass):
         cnx.commit()
         flash("アカウント作成に成功しました")
 
+# 新規アカウント作成
 @app.route("/create_user", methods=["GET", "POST"])
 def create_user():
     user_name, user_pass = user_registration_request()
@@ -41,6 +45,7 @@ def create_user():
 
     return redirect("/")
 
+# ログインページを表示
 @app.route("/show_login_page", methods=["GET", "POST"])
 def show_login_page():
     return render_template("login.html")
@@ -53,7 +58,6 @@ def login_user():
     user_pass = request.form.get("user_pass", "")
 
     get_user_information_query = f"SELECT user_name, password FROM user_table WHERE user_name = '{user_name}' AND password = '{user_pass}' "
-    # get_user_information_query = f"SELECT user_name, password, created_date FROM user_table WHERE user_name = '{user_name}', password = '{user_pass}' "
     cursor.execute(get_user_information_query)
     
     login_user = []
@@ -61,12 +65,15 @@ def login_user():
         profile = User(user_name, password)
         login_user.append(profile)
 
+    is_valid_login_user(user_pass, user_name, login_user)
+    return render_template("user_management.html", login_user=login_user)
 
+# アカウント名とパスワードが正しく入力されているかのチェック
+def is_valid_login_user(user_pass, user_name, login_user):
     if user_pass == "" or user_name == "":
         flash("アカウント名とパスワードを入力してください")
     elif len(login_user) == 0:
         flash("アカウント名かパスワードが間違っています", "")
     else:
         flash("ログイン成功！", "")
-
-    return render_template("user_management.html", login_user=login_user)
+    
